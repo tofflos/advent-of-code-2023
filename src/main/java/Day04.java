e74e47c0072dd15869231d89c554598b55d3e0e8
Erik Östlund <erik.ostlund@gmail.com>
2023-12-10 19:44:54 +0100

    Added solution for day 04 part 2.

--- src/main/java/Day04.java
+++ src/main/java/Day04.java
@@ -3,46 +3,62 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Function;
 import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
-public class Day04 {
+void main() throws IOException {
 
-    record Card(int id, int[] winners, int[] actuals) {
+    var cards = Files.lines(Paths.get("04.txt")).map(Card::of).toList();
 
-    }
+    var part1 = cards.stream()
+            .map(this::winners)
+            .mapToInt(winners -> (int) Math.pow(2, winners.size() - 1))
+            .sum();
 
-    public static void main(String[] args) throws IOException {
-        var pattern = Pattern.compile("\\d+");
+    System.out.println(STR."Part 01: \{part1}");
 
-        var part1 = Files.lines(Paths.get("04.txt"))
-                .map(line -> {
-                    var arr = line.split("[:|]");
+    var quantities = cards.stream().collect(Collectors.groupingBy(card -> card.id, Collectors.counting()));
 
-                    var matcher = pattern.matcher(arr[0]);
-                    matcher.find();
-                    var id = Integer.parseInt(matcher.group());
+    cards.forEach(card -> quantities.entrySet().stream()
+            .skip(card.id)
+            .limit(winners(card).size())
+            .forEach(entry -> entry.setValue(quantities.get(card.id) + entry.getValue())));
 
-                    matcher = pattern.matcher(arr[1]);
-                    var winners = matcher.results().map(MatchResult::group).mapToInt(Integer::parseInt).toArray();
+    var part2 = quantities.values().stream().mapToLong(Long::longValue).sum();
 
-                    matcher = pattern.matcher(arr[2]);
-                    var actuals = matcher.results().map(MatchResult::group).mapToInt(Integer::parseInt).toArray();
+    System.out.println(STR."Part 02: \{part2}");
+}
 
-                    return new Card(id, winners, actuals);
-                })
-                .mapToInt(card -> {
-                    var set1 = new HashSet<Integer>();
-                    Arrays.stream(card.winners).forEach(set1::add);
-                    var set2 = new HashSet<Integer>();
-                    Arrays.stream(card.actuals).forEach(set2::add);
+Set<Integer> winners(Card card) {
+    var set1 = new HashSet<Integer>();
+    Arrays.stream(card.winners).forEach(set1::add);
+    var set2 = new HashSet<Integer>();
+    Arrays.stream(card.actuals).forEach(set2::add);
 
-                    set1.retainAll(set2);
+    set1.retainAll(set2);
 
-                    return (int) Math.pow(2, set1.size() - 1);
-                })
-                .sum();
+    return set1;
+}
 
-        System.out.println(part1);
+record Card(int id, int[] winners, int[] actuals) {
+    static Pattern PATTERN = Pattern.compile("\\d+");
+
+    static Card of(String line) {
+        var arr = line.split("[:|]");
+
+        var matcher = PATTERN.matcher(arr[0]);
+        matcher.find();
+        var id = Integer.parseInt(matcher.group());
+
+        matcher = PATTERN.matcher(arr[1]);
+        var winners = matcher.results().map(MatchResult::group).mapToInt(Integer::parseInt).toArray();
+
+        matcher = PATTERN.matcher(arr[2]);
+        var actuals = matcher.results().map(MatchResult::group).mapToInt(Integer::parseInt).toArray();
+
+        return new Card(id, winners, actuals);
     }
 }
